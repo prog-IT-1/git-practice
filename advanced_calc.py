f321fafd71454f4545984a3f6c6d8a4e12bfa345
prog-IT-1 <darinasweg13@gmail.com>
2026-03-22 18:16:53 +0300

    Добавлен advanced_calc.py в ветку feature-calculator

---
+++ advanced_calc.py
@@ -0,0 +1,10 @@
+def multiply(a,b):
+    return a*b
+
+    def divide(a,b):
+        if b != 0:
+            return a / b
+        return "Ошибка: деление на ноль"
+
+        print(multiply(5,3))
+        print(divide(10,2))
